0afdfc58214d541fedeae24e877742f63f2b0107
Ema988 <emasarabiarojas567@gmail.com>
2023-09-28 21:54:45 -0600

    Ejercicio

--- practicas/P03/Ejercicio2.php
+++ practicas/P03/Ejercicio2.php
@@ -10,9 +10,9 @@
         <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
         <p>$a = “ManejadorSQL”;
            $b = 'MySQL';
-           $c = &$a;
+           $c = $a;
+           <br>
         </p>
-        <br>
         <?php
         $a = "ManejadorSQL";
         $b = 'MySQL';
@@ -46,7 +46,10 @@
         echo '<li>En el inciso "C" se vuelve a mostrar el contenido de las variables pero esta vez ya actualizadas con el nuevo valor que se les asigno en el incio "B"</li>';
         ?>
 
-
+<p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
 </body>
 
 </html>
--- practicas/P03/Ejercicio3.php
+++ practicas/P03/Ejercicio3.php
@@ -52,6 +52,10 @@
     $z[0] = "MySQL";
     print_r($z[0]);
     ?>
+    <p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
 
 
 </body>
