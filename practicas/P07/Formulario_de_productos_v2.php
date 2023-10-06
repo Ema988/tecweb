6f892f2068ff425d135cd694f3b64722b20c5759
Ema988 <emasarabiarojas567@gmail.com>
2023-10-06 02:59:42 -0600

    Ejercicios practica 7

---
+++ practicas/P07/Formulario_de_productos_v2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario de Registro de Productos</title>
+</head>
+<body>
+    <h1>Registro de Nuevos Productos</h1>
+    <form action="set_productosv2.php" method="post" enctype="multipart/form-data">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" required><br><br>
+
+        <label for="marca">Marca:</label>
+        <input type="text" name="marca" required><br><br>
+
+        <label for="modelo">Modelo:</label>
+        <input type="text" name="modelo" required><br><br>
+
+        <label for="precio">Precio:</label>
+        <input type="text" name="precio" required><br><br>
+
+        <label for="detalles">Detalles:</label>
+        <textarea name="detalles" required></textarea><br><br>
+
+        <label for="unidades">Unidades:</label>
+        <input type="number" name="unidades" required><br><br>
+
+        <label for="imagen">Imagen:</label>
+        <input type="file" name="imagen" accept="image/*" required><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
